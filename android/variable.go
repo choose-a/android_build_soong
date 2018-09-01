a8e30a037eedf4ffa18ccf1333efa0f9bfa1ad65
Stefan Berger <s.berger81@gmail.com>
2018-09-02 00:16:11 +0200

    soong: Move vendor init flag to soong
    Change-Id: I606ed027072697da986c70e75025ff03da764b72

--- android/variable.go
+++ android/variable.go
@@ -114,6 +114,11 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		Target_init_vendor_lib struct {
+			Cflags []string
+		}
+
 	} `android:"arch_variant"`
 }
 
@@ -230,6 +235,8 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	Target_init_vendor_lib *string `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
